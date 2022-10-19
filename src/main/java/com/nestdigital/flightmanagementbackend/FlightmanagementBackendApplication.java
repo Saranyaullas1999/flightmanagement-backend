d707635ce94b1acb9d8b092ffc288c34bd7d5f4d
Saranyaullas1999 <saranyaullas1999@gmail.com>
2022-10-19 15:18:23 +0530

    Flight management backend

---
+++ src/main/java/com/nestdigital/flightmanagementbackend/FlightmanagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.flightmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightmanagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightmanagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/flightmanagementbackend/FlightmanagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.flightmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightmanagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
